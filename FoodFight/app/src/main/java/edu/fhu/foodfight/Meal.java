c0c3ac6b7c61a4652f6e2b43eb82bbe27545b71f
Jesse Lloyd <jesse.a.lloyd@gmail.com>
2015-11-07 16:31:17 -0600

    Added initial Model classes
    Added basic Model classes for Fight, Meal, MealType (enum) and User.
    These contain the basic data attributes without any mutation/access
    methods. Just an initial commit to get the project up to speed.

---
+++ FoodFight/app/src/main/java/edu/fhu/foodfight/Meal.java
@@ -0,0 +1,16 @@
+package edu.fhu.foodfight;
+
+import android.media.Image;
+import java.util.Date;
+
+public class Meal {
+    // attributes
+
+    private Image image;
+    private int score;
+    private String description;
+    private MealType mealType;
+    private Date submissionDate;
+
+    // methods
+}
---
+++ FoodFight/app/src/main/java/edu/fhu/foodfight/User.java
@@ -0,0 +1,52 @@
+package edu.fhu.foodfight;
+
+import android.media.Image;
+import java.util.Date;
+
+/**
+ * Created by jesse on 11/7/15.
+ */
+
+public class User {
+    // attributes
+    private String username;
+    private Date joinDate;
+    private String userFirstName;
+    private String userLastName;
+    private Meal[] meals;
+    private Fight[] fights;
+    private int level;
+    private Image profilePicture;
+
+    private Fight[] currentFights;
+
+    private Fight[] finishedFights;
+
+    private Meal[] bestMeals;
+
+    private Meal[] worstMeals;
+
+    private int numberOfFinishedFights;
+
+    private int numberOfWins;
+
+    private int numberOfLosses;
+
+    private int numberOfTies;
+
+    private int averageScoreForMealType;
+
+    private int totalScore;
+
+    private int totalScoreForMonth;
+
+    private int totalScoreForWeek;
+
+    public User(String username, String firstname, String lastname, Image picture, int currentLevel) {
+        this.username = username;
+        this.userFirstName = firstname;
+        this.userLastName = lastname;
+        this.profilePicture = picture;
+        this.level = currentLevel;
+    }
+}
