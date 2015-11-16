769ed8c5c941ab905a638be5c7a395f2313ee511
averycupples <averycupples@Averys-MacBook-Pro-2.local>
2015-11-16 13:40:49 -0600

    Update Profile Layout
    - Create MLRoundedImage Class
    - Center Profile Image

--- FoodFight/app/src/main/java/edu/fhu/foodfight/ProfileFragment.java
+++ FoodFight/app/src/main/java/edu/fhu/foodfight/ProfileFragment.java
@@ -7,6 +7,8 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.*;
+import android.graphics.*;
 
 
 /**
@@ -54,17 +56,25 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        View myInflatedView = inflater.inflate(R.layout.fragment_profile, container,false);
+
+        TextView t = (TextView) myInflatedView.findViewById(R.id.profileName);
+
+
+        return myInflatedView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
